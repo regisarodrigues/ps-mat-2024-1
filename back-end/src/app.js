aae97dd3312e5f235ca7f2e2546a0efb23ea757b
Fausto G. Cintra <professor@faustocintra.com.br>
2024-03-06 09:16:19 -0300

    (06/03) CRUD para o model User

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 
 import indexRouter from "./routes/index.js";
-import usersRouter from "./routes/users.js";
+//import usersRouter from "./routes/users.js";
 
 const app = express();
 
@@ -13,13 +13,16 @@ app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+//app.use("/users", usersRouter);
 
 /************************************************
  * ROTAS DA API
 ************************************************/
 
-import carRouter from './routes/car.js'
-app.use('/cars', carRouter)
+import carRoute from './routes/car.js'
+app.use('/cars', carRoute)
+
+import userRoute from './routes/user.js'
+app.use('/users', userRoute)
 
 export default app;
--- back-end/src/routes/user.js
+++ back-end/src/routes/user.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import controller from '../controllers/car.js'
+import controller from '../controllers/user.js'
 
 const router = Router()
 
