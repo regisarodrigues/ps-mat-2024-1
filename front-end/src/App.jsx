6f4d2d922f2eb6e14b18dbed5315249137471404
LINIKER ELIAS ALVES DA SILVA <1090482211038@fatec-srv.local>
2024-03-27 11:00:29 -0300

    (27/03) Rodapé, navegações e página de login

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -8,16 +8,28 @@ import './App.css'
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './ui/theme';
 import CssBaseline from "@mui/material/CssBaseline";
+import Box from '@mui/material/Box'
+
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
 
 import TopBar from './ui/TopBar';
+import FooterBar from './ui/FooterBar';
+
 
 function app() {
 
   return (
     <>
       <ThemeProvider theme={theme}> 
-        <CssBaseline />
-        <TopBar />
+        <BrowserRouter>
+          <CssBaseline />
+          <TopBar />
+          <Box sx={{ margin: '24px 24 px 72px 24px'}}>
+            <AppRoutes />
+          </Box>
+          <FooterBar />
+        </BrowserRouter>
       </ThemeProvider>
     </>
   )
---
+++ front-end/src/ui/FooterBar.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import CoffeeIcon from '@mui/icons-material/Coffee';
+
+export default function FooterBar() {
+    return (
+        <Toolbar
+        variant="dense"
+        component="footer"
+            sx={{
+                position: 'fixed',
+                bottom: 0,
+                justifyContent: 'center',
+                width: '100vw',
+                backgroundColor: 'action.disabledBackground'
+            }}
+        >
+            <Typography variant="caption"
+                sx={{
+                    '& a' : {
+                        color: 'secondary.light'
+                    }
+                }}
+            >
+                Desenvolvido com <CoffeeIcon fontsize="small" /> por <a href="mailto:Liniker.silva03@gmail.com"> Liniker Elias </a>, 2024
+
+            </Typography>
+        </Toolbar>
+    )
+}
