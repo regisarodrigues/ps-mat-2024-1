c699854db56ea0582fc10fbb3405867162fad7d1
Fausto G. Cintra <professor@faustocintra.com.br>
2024-05-08 10:56:50 -0300

    (08/05) Término e funcionamento do cadastro de clientes

--- back-end/src/controllers/customer.js
+++ back-end/src/controllers/customer.js
@@ -9,7 +9,7 @@ controller.create = async function (req, res) {
     await prisma.customer.create({ data: req.body })
 
     // HTTP 201: Created
-    res.status(201).end()
+    res.status(201).send({ message: 'Created' }).end()
   }
   catch(error) {
     console.log(error)
--- front-end/src/pages/customer/CustomerForm.jsx
+++ front-end/src/pages/customer/CustomerForm.jsx
@@ -1,35 +1,158 @@
 import React from 'react'
 import Typography from '@mui/material/Typography'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 import InputMask from 'react-input-mask'
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
-import ptLocale from 'date-fns/locale/pt-BR'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
+import { ptBR } from 'date-fns/locale/pt-BR'
 import { parseISO } from 'date-fns'
+import MenuItem from '@mui/material/MenuItem'
+import Button from '@mui/material/Button'
+import useConfirmDialog from '../../ui/useConfirmDialog'
+import useNotification from '../../ui/useNotification'
+import useWaiting from '../../ui/useWaiting'
+import myfetch from '../../lib/myfetch'
 
 export default function CustomerForm() {
 
+  /*
+    Por padrão, todos os campos do nosso formulário terão como
+    valor inicial uma string vazia. A exceção é o campo birth_date
+    que, devido ao funcionamento do componente DatePicker, deve
+    iniciar valendo null.
+  */
+  const formDefaults = {
+    name: '',
+    ident_document: '',
+    birth_date: null,
+    street_name: '',
+    house_number: '',
+    complements: '',
+    municipality: '',
+    state: '',
+    phone: '',
+    email: ''
+  }
+
   const [state, setState] = React.useState({
-    customer: {}
+    customer: { ...formDefaults },
+    formModified: false
   })
   const {
-    customer
+    customer,
+    formModified
   } = state
 
   const params = useParams()
+  const navigate = useNavigate()
 
-  function handleFieldChange(event) {}
+  const { askForConfirmation, ConfirmDialog } = useConfirmDialog()
+  const { notify, Notification } = useNotification()
+  const { showWaiting, Waiting } = useWaiting()
+
+  const states = [
+    { value: 'DF', label: 'Distrito Federal' },
+    { value: 'ES', label: 'Espírito Santo' },
+    { value: 'GO', label: 'Goiás' },
+    { value: 'MS', label: 'Mato Grosso do Sul' },
+    { value: 'MG', label: 'Minas Gerais' },
+    { value: 'PR', label: 'Paraná' },
+    { value: 'RJ', label: 'Rio de Janeiro' },
+    { value: 'SP', label: 'São Paulo' }
+  ]
+
+  const phoneMaskFormatChars = {
+    '9': '[0-9]',    // somente dígitos
+    '%': '[\s0-9]'   // dígitos ou espaço em branco (\s)
+  }
+
+  function handleFieldChange(event) {
+    const customerCopy = { ...customer }
+    customerCopy[event.target.name] = event.target.value
+    setState({ ...state, customer: customerCopy, formModified: true })
+  }
+
+  async function handleFormSubmit(event) {
+    event.preventDefault()      // Evita que a página seja recarregada
+    showWaiting(true)       // Exibe a tela de espera
+    try {
+      // Se houver parâmetro na rota, significa que estamos modificando
+      // um cliente já existente. A requisição será enviada ao back-end
+      // usando o método PUT
+      if(params.id) await myfetch.put(`/customers/${params.id}`, customer)
+      // Caso contrário, estamos criando um novo cliente, e enviaremos
+      // a requisição com o método POST
+      else await myfetch.post('/customers', customer)
+
+      // Deu certo, vamos exbir a mensagem de feedback que, quando for
+      // fechada, vai nos mandar de volta para a listagem de clientes
+      notify('Item salvo com sucesso.', 'success', 4000, () => {
+        navigate('..', { relative: 'path', replace: true })
+      })
+    }
+    catch(error) {
+      console.error(error)
+      notify(error.message, 'error')
+    }
+    finally {
+      // Desliga a tela de espera, seja em caso de sucesso, seja em caso de erro
+      showWaiting(false)
+    }
+  }
+
+  /*
+    useEffect() que é executado apenas uma vez, no carregamento do componente.
+    Verifica se a rota tem parâmetro. Caso tenha, significa que estamos vindo
+    do componente de listagem por meio do botão de editar, e precisamos chamar
+    a função loadData() para buscar no back-end os dados do cliente a ser editado
+  */
+  React.useEffect(() => {
+    if(params.id) loadData()
+  }, [])
+
+  async function loadData() {
+    showWaiting(true)
+    try {
+      const result = await myfetch.get(`/customers/${params.id}`)
+
+      // Converte o formato de data armazenado no banco de dados
+      // para o formato reconhecido pelo componente DatePicker
+      result.birth_date = parseISO(result.birth_date)
+
+      setState({ ...state, customer: result })
+    }
+    catch(error) {
+      console.error(error)
+      notify(error.message, 'error')
+    }
+    finally {
+      showWaiting(false)
+    }
+  }
+
+  async function handleBackButtonClick() {
+    if(formModified &&
+       ! await askForConfirmation('Há informações não salvas. Deseja realmente sair?')
+    ) return  // Sai da função sem fazer nada
+    
+    // Navega de volta para a página de listagem
+    navigate('..', { relative: 'path', replace: true })
+  }
 
   return (
     <>
+      <ConfirmDialog />
+      <Notification />
+      <Waiting />
+      
       <Typography variant="h1" gutterBottom>
         { params.id ? `Editar cliente #${params.id}` : 'Cadastrar novo cliente' }
       </Typography>
 
       <Box className="form-fields">
-        <form>
+        <form onSubmit={handleFormSubmit}>
 
           <TextField 
             name="name"
@@ -37,7 +160,6 @@ export default function CustomerForm() {
             variant="filled"
             required
             fullWidth
-            autoFocus
             value={customer.name}
             onChange={handleFieldChange}
           />
@@ -55,7 +177,130 @@ export default function CustomerForm() {
                 fullWidth
               /> 
             }
-          </InputMask>                   
+          </InputMask>
+
+          <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>
+            <DatePicker 
+              label="Data de nascimento"
+              value={customer.birth_date}
+              onChange={ value => handleFieldChange({
+                target: { name: 'birth_date', value }
+              })}
+              slotProps={{
+                textField: {
+                  variant: 'filled',
+                  fullWidth: true
+                }
+              }}
+            />
+          </LocalizationProvider>
+
+          <TextField 
+            name="street_name"
+            label="Logradouro"
+            placeholder="Rua, av., travessa, etc."
+            variant="filled"
+            required
+            fullWidth
+            value={customer.street_name}
+            onChange={handleFieldChange}
+          />   
+
+          <TextField 
+            name="house_number"
+            label="nº"
+            variant="filled"
+            required
+            fullWidth
+            value={customer.house_number}
+            onChange={handleFieldChange}
+          />  
+
+          <TextField 
+            name="complements"
+            label="Complemento"
+            placeholder="Ap., bloco, casa, etc."
+            variant="filled"
+            fullWidth
+            value={customer.complements}
+            onChange={handleFieldChange}
+          />
+
+          <TextField 
+            name="municipality"
+            label="Município"
+            variant="filled"
+            required
+            fullWidth
+            value={customer.municipality}
+            onChange={handleFieldChange}
+          />
+
+          <TextField
+            name="state"
+            label="UF"
+            variant="filled"
+            required
+            fullWidth
+            value={customer.state}
+            onChange={handleFieldChange}
+            select
+          >
+            {
+              states.map(s => 
+                <MenuItem key={s.value} value={s.value}>
+                  {s.label}
+                </MenuItem>
+              )
+            }
+          </TextField>     
+
+          <InputMask
+            mask="(99) %9999-9999"
+            formatChars={phoneMaskFormatChars}
+            maskChar=" "
+            value={customer.phone}
+            onChange={handleFieldChange}
+          >
+            { () => <TextField 
+                name="phone"
+                label="Telefone/celular"
+                variant="filled"
+                required
+                fullWidth
+              /> 
+            }
+          </InputMask>
+
+          <TextField 
+            name="email"
+            label="E-mail"
+            variant="filled"
+            required
+            fullWidth
+            value={customer.email}
+            onChange={handleFieldChange}
+          />  
+
+          <Box sx={{ display: 'flex', justifyContent: 'space-around', width: '100%' }}>
+            <Button
+              variant="contained"
+              color="secondary"
+              type="submit"
+            >
+              Salvar
+            </Button>
+            <Button
+              variant="outlined"
+              onClick={handleBackButtonClick}
+            >
+              Voltar
+            </Button>
+          </Box> 
+
+          <Box sx={{ fontFamily: 'monospace', display: 'flex', width: '100%' }}>
+            {JSON.stringify(customer)}
+          </Box>
 
         </form>
       </Box>
